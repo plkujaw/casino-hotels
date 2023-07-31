b738fe89a4b2e4eb454650442e0042585a18adce
Kuba Jawor <kuba@lattimoreandfriends.com>
2023-07-31 08:41:03 +0100

    Hero Section

--- inc/template-parts/hero.php
+++ inc/template-parts/hero.php
@@ -10,7 +10,7 @@ $subtitle = get_field('hero_subtitle');
       <a href="<?php site_url() ?>"><img src="<?php echo get_template_directory_uri() . '/assets/img/logo.png' ?>" width="148" height="148"></a>
       <h1 class="hero__title"><?php echo $title; ?></h1>
       <h2 class="hero__subtitle"><?php echo $subtitle; ?></h2>
-      <a href="#about" class="btn">Explore Today</a>
+      <a href="#about" class="hero__cta btn">Explore Today</a>
     </div>
   </div>
   <div class="hero__background">
